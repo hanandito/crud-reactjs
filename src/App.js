0dfc7e0c806314928115409fd73e2397a8322e05
teddypicker <hananditos@gmail.com>
2021-02-08 14:02:20 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import NavbarComponent from './components/NavbarComponent'
 
 export default class App extends Component {
   render() {
     return(
-      <div>
-        <h1>Test</h1>
-      </div>
+      <NavbarComponent />
     )
   }
 }
