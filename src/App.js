7d70f035c12681f6948a57f83cd1772bf84e2f59
teddypicker <hananditos@gmail.com>
2021-02-08 16:44:44 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,27 @@ export default class App extends Component {
         alamat: "pondok bamboe",
         umur: 88,
         nohp: "0812101010",
+      },
+      {
+        id : 3,
+        nama : "torik",
+        alamat: "limo",
+        umur: 20,
+        nohp: "08223979",
+      },
+      {
+        id : 4,
+        nama : "miko",
+        alamat: "tanah sereal",
+        umur: 24,
+        nohp: "0812108",
+      },
+      {
+        id : 5,
+        nama : "arie",
+        alamat: "serua",
+        umur: 22,
+        nohp: "08192198",
       }
     ]
   }
