819d6fb56ac3761983a269f050e6abfe77641996
teddypicker <hananditos@gmail.com>
2021-02-08 14:22:20 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import NavbarComponent from './components/NavbarComponent'
 
 export default class App extends Component {
+  state = {
+    title : "hanandev"
+  }
   render() {
     return(
       <NavbarComponent />
